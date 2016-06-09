f2a65b509f59b2deabca51632eb530f8908e8377
Roger Clark <roger@rogerclark.net>
2016-06-10 09:54:52 +1000

    Updated convert-2d.sh with new version from @Vincent14

--- convert-2d.sh
+++ convert-2d.sh
@@ -1,4 +1,30 @@
-openscad $1 -D generate=1 -o $1.csg 2>&1 >/dev/null  | sed 's/ECHO: \"\[LC\] //' | sed 's/"$//' | sed '$a;' >./2d_$1
-sed -i "1iuse <lasercut.scad>; \$fn=60; \n projection(cut = false) \n"  ./2d_$1
-openscad ./2d_$1 -o ./2d_$1.dxf
+# Defines where OpenSCAD is installed
 
+openscad_bin() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD $1
+  else
+    openscad $1
+  fi
+}
+
+
+# Creates a 2d plan of your 3D model
+
+openscad_bin "$1 -D generate=1 -o /tmp/$1.csg" 2>&1 >/dev/null | sed -e 's/ECHO: \"\[LC\] //' -e 's/"$//' -e '$a\
+;' >./2d_$1
+
+sed -i.tmp '1 i\
+use <lasercut.scad>;\
+\$fn=60;\
+projection(cut = false)\
+' ./2d_$1
+
+rm 2d_$1.tmp
+
+# Exports in others formats (could be very long)
+
+#openscad_bin "./2d_$1 -o ./2d_$1.csg"
+#openscad_bin "./2d_$1 -o ./2d_$1.dxf"
+#openscad_bin "./2d_$1 -o ./2d_$1.svg"
+#openscad_bin "./$1 -o ./3d_$1.stl"
