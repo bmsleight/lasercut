1195a4dd6aa006613e0919f5e9cd680cfa93b74a
Denis Klimov <klimov.d.e@gmail.com>
2022-11-23 13:38:30 +0500

    Improve and simplification convert-2d.sh. Added support command line options for it.

--- convert-2d.sh
+++ convert-2d.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+function usage() {
+  echo "convert-2d.sh <SCAD_FILE> [dxf|svg|csg|stl]"
+}
+
 # Defines where OpenSCAD is installed
 openscad_bin() {
-  set +x
   if [ -n "$OPENSCAD_BIN" ]
   then
     $OPENSCAD_BIN $1
@@ -15,30 +18,39 @@ openscad_bin() {
 }
 
 SCAD_FILE=$1
+if [ -z "$SCAD_FILE" ]; then
+    usage 
+    exit 1
+fi
+if [ ! -r "$SCAD_FILE" ]; then 
+    echo "Can not read file $SCAD_FILE"
+    exit 1
+fi
+
+EXT=$2
+[ -z "$EXT" ] && EXT=dxf
+
 SCAD_FILE_WO_EXT=${SCAD_FILE%.*}
-
-# Creates a 2d plan of your 3D model
-TMPCSG=/tmp/$SCAD_FILE_WO_EXT.csg
-TMPSCAD=/tmp/$SCAD_FILE_WO_EXT.scad
 SCAD_2D_FILE="${SCAD_FILE_WO_EXT}_2d.scad"
+OUTPUT_FILE="${SCAD_FILE_WO_EXT}.${EXT}"
 
-openscad_bin "$1 -D generate=1 -o $TMPCSG" 2>&1 >/dev/null | \
-        sed -e 's/ECHO: \"\[LC\] //' -e 's/"$//' -e '$a\;' -e  '/WARNING/d' -e '/Using default value/d' -e '/set +x/d'  >$TMPSCAD
+openscad_bin "$1 -D generate=1 --export-format csg -o /dev/null" 2>&1 | \
+        sed -e 's/ECHO: \"\[LC\] //' \
+        -e 's/"$//' \
+        -e '$a\;' \
+        -e  '/WARNING/d' \
+        -e '/Using default value/d' \
+        -e '/^+/d' > $SCAD_2D_FILE
 
-sed -i.tmp '1 i\
+sed -i '1 i\
 // May need to adjust location of <lasercut.scad> \
 use <lasercut.scad>	;\
 \$fn=60;\
 projection(cut = false)\
-' $TMPSCAD
-
-mv $TMPSCAD $SCAD_2D_FILE
-
-# Exports in others formats (could be very long)
-#openscad_bin "./$SCAD_2D_FILE -o ${SCAD_FILE_WO_EXT}.stl"
-#openscad_bin "./$SCAD_2D_FILE -o ${SCAD_FILE_WO_EXT}.csg"
-#openscad_bin "./$SCAD_2D_FILE -o ${SCAD_FILE_WO_EXT}.svg"
-openscad_bin "./$SCAD_2D_FILE -o ${SCAD_FILE_WO_EXT}.dxf"
+' $SCAD_2D_FILE
 
 
+openscad_bin "$SCAD_2D_FILE -o $OUTPUT_FILE"
 
+echo "Output files:"
+ls -l "$OUTPUT_FILE" "$SCAD_2D_FILE"
